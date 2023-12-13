cb2521be98f50bf2fb4a84cf4c6fc23ce357cf47
JJThomtin <129408043+JJThomtin@users.noreply.github.com>
2023-12-13 14:07:28 +0200

    Implemented Clear Button (#7)

--- javascript.js
+++ javascript.js
@@ -1,14 +1,23 @@
 const canvas = document.getElementById("canvas");
-
+const clear_btn = document.getElementById("clear-btn");
 function makeRows(rows, cols) {
     canvas.style.setProperty('--grid-rows', rows);
     canvas.style.setProperty('--grid-cols', cols);
-    canvas.style.setProperty('--gridsize', 700/cols)
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
       canvas.appendChild(cell).className = "grid-item";
     };
-  };
-  
-  makeRows(100, 100);
+};
+
+clear_btn.onclick = function() {
+    document.querySelectorAll(".grid-item").forEach((p) => {
+        p.style.backgroundColor = "white"
+    })
+}
+
+
+window.onload = () => {
+    makeRows(100, 100)
+}
+
   
